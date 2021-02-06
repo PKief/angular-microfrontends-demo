9f38afadcfd9a6576b806d1ea9879dfc6ebc19ab
PKief <philipp.kief@gmx.de>
2021-02-06 21:35:45 +0100

    Remove unused project directory

--- mf2/projects/shared-lib/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of shared-lib
- */
-
-export * from './lib/shared-lib.service';
-export * from './lib/shared-lib.component';
-export * from './lib/shared-lib.module';
-export * from './lib/other/other.component';
